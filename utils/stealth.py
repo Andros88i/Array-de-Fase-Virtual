ae761f9d02ada20cb8fee7ad36a5c142da921af1
Andros88i <androsgil527@Gmail.com>
2026-01-16 11:22:53 -0600

    Add OPSECManager class with traffic masking method

--- utils/stealth.py
+++ utils/stealth.py
@@ -1,1 +1,11 @@
+# utils/stealth.py
+import dns.resolver
+import random
 
+class OPSECManager:
+    def __init__(self):
+        pass
+    
+    def mask_traffic(self, data: bytes) -> bytes:
+        """Enmascara tráfico para parecer legítimo"""
+        pass
