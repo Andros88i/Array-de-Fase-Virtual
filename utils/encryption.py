d8a78207d9226b2471b15526e9e68f2b2805379d
Andros88i <androsgil527@Gmail.com>
2026-01-16 11:22:30 -0600

    Implement SecureComms class for message encryption
    Add SecureComms class with message encryption method stub

--- utils/encryption.py
+++ utils/encryption.py
@@ -1,1 +1,12 @@
+# utils/encryption.py
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+from cryptography.hazmat.primitives.asymmetric import x25519
+import os
 
+class SecureComms:
+    def __init__(self):
+        pass
+    
+    def encrypt_message(self, message: bytes, key: bytes) -> bytes:
+        """Cifra mensaje con ChaCha20-Poly1305"""
+        pass
