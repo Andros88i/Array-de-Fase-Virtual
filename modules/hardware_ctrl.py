5ee8bbd709cdadc81e96e4bfc816f9afd44a73cb
Andros88i <androsgil527@Gmail.com>
2026-01-16 11:21:14 -0600

    Implement RTLSDRController class structure
    Added RTLSDRController class with initialization and method stub.

--- modules/hardware_ctrl.py
+++ modules/hardware_ctrl.py
@@ -1,1 +1,11 @@
+# modules/hardware_ctrl.py
+import subprocess
+import os
 
+class RTLSDRController:
+    def __init__(self):
+        self.rtl_path = "/data/data/com.termux/files/usr/bin/rtl_sdr"
+    
+    def overclock_to_band(self, target_freq: float) -> bool:
+        """Configura RTL-SDR para banda Ku"""
+        pass
